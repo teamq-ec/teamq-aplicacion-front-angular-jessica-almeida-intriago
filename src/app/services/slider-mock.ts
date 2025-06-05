c2e071f0702b0877fe1fd1fe9b6bc85956c68b6b
jalmeida <jalmeida@viamatica.com>
2025-06-05 09:32:47 -0500

    Finalizacion de la prueba

--- src/app/services/slider-mock.ts
+++ src/app/services/slider-mock.ts
@@ -29,7 +29,7 @@ export class SliderMock {
       headline: 'Lorem Ipsun 3.',
       text: 'Sunt anim mollit velit incididunt reprehenderit consectetur cupidatat labore. Tempor Lorem et est aliquip excepteur id cupidatat ut cupidatat officia in sint. Magna aliqua mollit qui do aliqua minim aliquip fugiat ut magna Lorem enim. In labore consectetur duis aute in reprehenderit esse aliquip ex ut nisi.',
       buttonText: 'Aprender Más',
-      buttonAction: () => alert('Clicked Aprender Más'),
+      buttonAction: () => alert('Clicked Aprender Más'), 
     },
   ]);
 }
